2d9ae8f1515c541d0040261292e2267748e4dfd1
Govind Menon <govindmenon1995@yahoo.co.in>
2017-03-05 19:41:54 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(arr)
+  i=0
+  while i<arr.length do
+    row = " #{arr[i]} | #{arr[i+1]} | #{arr[i+2]} "
+    del = "-----------"
+    puts row
+    if i!=6
+      puts del
+    end
+    i+=3
+  end
+end
